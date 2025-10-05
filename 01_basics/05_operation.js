5e03329ddfcc6cbcc4b273904f1c50cb5bd88248
Aditya sharma <codebyaditya143@gmail.com>
2025-10-05 09:58:21 +0000

    learn operation and concatenation of string

---
+++ 01_basics/05_operation.js
@@ -0,0 +1,25 @@
+// let value  = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(3+3);          // Addition
+// console.log(5-3);          // Subtraction
+// console.log(3*3);          // Multiplication
+// console.log(6/3);          // division
+// console.log(2**5);         // 2 to power 5
+// console.log(37%3);          // Modulus ---> gives remainder in ans
+
+// let str1 = "Aditya "
+// let str2 = "Sharma"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// String and Number Concatenation in JavaScript
+
+console.log("1" + 1);         // "11" → "1" (string) + 1 (number) → "11"
+console.log(1 + "3");         // "13" → 1 (number) + "3" (string) → "13"
+console.log("1" + "3");       // "13" → "1" + "3" → both are strings
+console.log("1" + 3 + 3);     // "133" → "1" + 3 = "13", then "13" + 3 = "133"
+console.log(1 + 3 + "3");     // "43" → 1 + 3 = 4 (number addition), then 4 + "3" = "43"
+
+
